c3bc027cd225c7313c255954e423a05d30b357ae
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-02-18 04:04:37 +0100

    Make test test bad requests (404s)

--- lib/test.ts
+++ lib/test.ts
@@ -4,23 +4,42 @@ import * as osu from "./index"
 const key = process.env.KEY
 if (key === undefined) {throw new Error("The API key has not been defined in the environment variables! (name of the variable is `KEY`)")}
 const api = new osu.API(key, true)
+const bad_id = -1
 
 const test: () => Promise<void> = async () => {
-	let match: osu.Match = await api.getMatch(103845156)
-	console.log("Name of the match:", match.match.name, "\n")
+	console.log("\nTesting: getUser()")
+	const user_id = 7276846
+	let u1: osu.User = await api.getUser({user_id}, 0)
+	if (u1.user_id !== user_id) {
+		throw new Error(`The user's id is not what it should be!
+		Expected: ${user_id}
+		Got: ${u1.user_id}`)
+	}
+	await api.getUser({user_id: bad_id}, 3)
+	await api.getUser({user_id: bad_id}, bad_id)
 
-	const game = match.games[1]
-	const score = game.scores.find((s) => s.user_id === 7276846)!
+	console.log("\nTesting: getMatch()")
+	const match_name = "IT: (tout le monde) vs (Adéquat feur)"
+	let m1: osu.Match = await api.getMatch(106369699)
+	if (m1.match.name !== match_name) {
+		throw new Error(`The match's name is not what it should be!
+		Expected: ${match_name}
+		Got: ${m1.match.name}`)
+	}
+	await api.getMatch(bad_id)
 
-	let beatmap: osu.Beatmap = await api.getBeatmap(game.beatmap_id, osu.Mods["DoubleTime"])
-	console.log("First map outside warmups was:", `${beatmap.artist} - ${beatmap.title} [${beatmap.version}],`, `a ${beatmap.getGenre()} song\n`)
+	console.log("\nTesting: getBeatmap()")
+	const song_name = "FriendZoned"
+	let b1: osu.Beatmap = await api.getBeatmap(m1.games[1].beatmap_id, 0)
+	if (b1.title !== song_name) {
+		throw new Error(`The beatmap's song name is not what it should be!
+		Expected: ${song_name}
+		Got: ${b1.title}`)
+	}
+	await api.getBeatmap(bad_id, 3)
+	await api.getBeatmap(bad_id, bad_id)
 
-	let user: osu.User = await api.getUser({user_id: score.user_id}, 0)
-	console.log(`${user.username}, who is currently ranked #${user.pp_rank} on osu!, got a score of ${score.score} on it!\n`)
-
-	let scores: osu.Score[] = await api.getUserScores(user, 0, "best", 3)
-	console.log("This user's top 3 scores are worth in pp respectively:")
-	scores.forEach((s) => console.log(s.pp))
+	// TODO: Check beatmap stats when mods are active
 
 	console.log("\nLooks like the test went well!")
 }
