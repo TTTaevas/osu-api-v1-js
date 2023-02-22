040b2127b49e9db47cb6cd8c379c7d6e9c73a41b
Taevas <67872932+TTTaevas@users.noreply.github.com>
2023-02-22 19:30:35 +0100

    Create `Replay` and `getReplay`

---
+++ lib/replay.ts
@@ -0,0 +1,13 @@
+/**
+ * You might wanna check out https://osu.ppy.sh/wiki/en/Client/File_formats/Osr_%28file_format%29
+ */
+export interface Replay {
+	/**
+	 * Encoded LZMA stream
+	 */
+	content: string,
+	/**
+	 * The encoding `content` uses, should always be "base64"
+	 */
+	encoding: string
+}
